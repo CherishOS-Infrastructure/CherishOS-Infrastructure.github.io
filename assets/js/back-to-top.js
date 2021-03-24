674ae98f3ad6e7e36613774a681ccbe6dfa7e965
Hưng Phan <phandinhhungvp2001@gmail.com>
2021-03-24 16:21:44 +0700

    website:Add back-to-top
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- assets/js/back-to-top.js
+++
@@ -1,8 +0,0 @@
-const backtotop=document.querySelector("back-to-top");
-window.addEventListener("scroll",()=>{
-  if(window.pageYOffset >100){
-  backtotop.classList.add("active");
-}else{
-  backtotop.classList.remove("active");
-}
-})
