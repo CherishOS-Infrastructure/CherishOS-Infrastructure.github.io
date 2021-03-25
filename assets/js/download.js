9a9da8e80ce33676c2930dc7685e83d1ee4f062d
huy29012001 <huy29012001@gmail.com>
2021-03-25 11:07:15 +0700

    add action for download
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

---
+++ assets/js/download.js
@@ -0,0 +1,36 @@
+document.getElementById("btnAsus").onclick = function () {
+    document.getElementById("asusdev").style.display = '';
+    document.getElementById("lenovodev").style.display = 'none';
+    document.getElementById("oneplusdev").style.display = 'none';
+    document.getElementById("realmedev").style.display = 'none';
+    document.getElementById("xiaomidev").style.display = 'none';
+};
+
+document.getElementById("btnLenovo").onclick = function () {
+    document.getElementById("asusdev").style.display = 'none';
+    document.getElementById("lenovodev").style.display = '';
+    document.getElementById("oneplusdev").style.display = 'none';
+    document.getElementById("realmedev").style.display = 'none';
+    document.getElementById("xiaomidev").style.display = 'none';
+};
+document.getElementById("btnOneplus").onclick = function () {
+    document.getElementById("asusdev").style.display = 'none';
+    document.getElementById("lenovodev").style.display = 'none';
+    document.getElementById("oneplusdev").style.display = '';
+    document.getElementById("realmedev").style.display = 'none';
+    document.getElementById("xiaomidev").style.display = 'none';
+};
+document.getElementById("btnRealme").onclick = function () {
+    document.getElementById("asusdev").style.display = 'none';
+    document.getElementById("lenovodev").style.display = 'none';
+    document.getElementById("oneplusdev").style.display = 'none';
+    document.getElementById("realmedev").style.display = '';
+    document.getElementById("xiaomidev").style.display = 'none';
+};
+document.getElementById("btnXiaomi").onclick = function () {
+    document.getElementById("asusdev").style.display = 'none';
+    document.getElementById("lenovodev").style.display = 'none';
+    document.getElementById("oneplusdev").style.display = 'none';
+    document.getElementById("realmedev").style.display = 'none';
+    document.getElementById("xiaomidev").style.display = '';
+};
